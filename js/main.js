6348da6ad4a947422642f0bc63f944b9f2641aba
revgodgod74-glitch <revgodgod74@gmail.com>
2026-01-17 11:23:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 let coins = 0;
+let power = 1;
 
 function earn() {
-  coins += 1;
-  document.getElementById("coins").innerText = "Coins: " + coins;
+  coins += power;
+  updateUI();
 }
+
+function boost() {
+  power = 2;
+  setTimeout(() => power = 1, 10000);
+}
+
+function updateUI() {
+  document.getElementById("coins").innerText = "Coins: " + coins;
+  document.getElementById("bar").style.width = (coins % 100) + "%";
+  document.getElementById("level").innerText =
+    "Level " + (Math.floor(coins / 100) + 1);
+}
+
+function playMusic() {
+  document.getElementById("bgMusic").play();
+}
+
+window.onload = () => {
+  if (!localStorage.getItem("bonus")) {
+    document.getElementById("bonusPopup").classList.add("show");
+  }
+};
+
+function claimBonus() {
+  coins += 10;
+  localStorage.setItem("bonus", "yes");
+  document.getElementById("bonusPopup").classList.remove("show");
+  updateUI();
+}
+
+document.addEventListener("mousemove", e => {
+  let c = document.getElementById("cursor");
+  c.style.left = e.pageX + "px";
+  c.style.top = e.pageY + "px";
+});
