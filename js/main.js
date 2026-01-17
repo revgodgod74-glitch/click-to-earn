6748b7e184855e665909488cdeb08a98bcb8cf3a
revgodgod74-glitch <revgodgod74@gmail.com>
2026-01-17 11:28:38 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
-let coins = 0;
+let coins = Number(localStorage.getItem("coins")) || 0;
 let power = 1;
 
-function earn() {
-  coins += power;
-  updateUI();
+updateUI();
+
+function earn(){
+coins += power;
+save();
 }
 
-function boost() {
-  power = 2;
-  setTimeout(() => power = 1, 10000);
+function boost(){
+power = 2;
+setTimeout(()=>power=1,10000);
 }
 
-function updateUI() {
-  document.getElementById("coins").innerText = "Coins: " + coins;
-  document.getElementById("bar").style.width = (coins % 100) + "%";
-  document.getElementById("level").innerText =
-    "Level " + (Math.floor(coins / 100) + 1);
+function save(){
+localStorage.setItem("coins", coins);
+updateUI();
 }
 
-function playMusic() {
-  document.getElementById("bgMusic").play();
+function updateUI(){
+document.getElementById("coins").innerText="Coins: "+coins;
+document.getElementById("bar").style.width=(coins%100)+"%";
+document.getElementById("level").innerText="Level "+(Math.floor(coins/100)+1);
 }
 
-window.onload = () => {
-  if (!localStorage.getItem("bonus")) {
-    document.getElementById("bonusPopup").classList.add("show");
-  }
+function playMusic(){
+document.getElementById("bgMusic").play();
+}
+
+window.onload=()=>{
+if(!localStorage.getItem("bonus")){
+document.getElementById("bonusPopup").classList.add("show");
+}
 };
 
-function claimBonus() {
-  coins += 10;
-  localStorage.setItem("bonus", "yes");
-  document.getElementById("bonusPopup").classList.remove("show");
-  updateUI();
+function claimBonus(){
+coins+=20;
+localStorage.setItem("bonus","yes");
+document.getElementById("bonusPopup").classList.remove("show");
+save();
 }
 
-document.addEventListener("mousemove", e => {
-  let c = document.getElementById("cursor");
-  c.style.left = e.pageX + "px";
-  c.style.top = e.pageY + "px";
+document.addEventListener("mousemove",e=>{
+let c=document.getElementById("cursor");
+c.style.left=e.pageX+"px";
+c.style.top=e.pageY+"px";
 });
